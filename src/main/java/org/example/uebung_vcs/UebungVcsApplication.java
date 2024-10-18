ba45bf6258eb0a689dda274facef984a43e1c3b9
Max Tepper <FlashwarLP@gmail.com>
2024-10-18 13:11:06 +0200

    -inital push

---
+++ src/main/java/org/example/uebung_vcs/UebungVcsApplication.java
@@ -0,0 +1,13 @@
+package org.example.uebung_vcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebungVcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UebungVcsApplication.class, args);
+    }
+
+}
